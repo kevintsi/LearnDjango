24bd7e77778ac4b4812f3649bfc8cd6bec8a9fe5
Kévin TSI <kevtsiii@gmail.com>
2022-09-12 12:12:39 +0200

    Mise en place d'un formulaire en utilisant le formulaire intégré à django

--- mysite/authcustom/views.py
+++ mysite/authcustom/views.py
@@ -3,20 +3,25 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate
 from django.core.exceptions import ValidationError
 
+from authcustom.forms import LoginForm
+
 # Create your views here.
 
 
 def login(request):
     try:
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            return HttpResponseRedirect(reverse('polls:index'))
+        if request.method == 'POST':
+            form = LoginForm(request.POST)
+            if form.is_valid():
+                username = form.cleaned_data['username']
+                password = form.cleaned_data['password']
+                user = authenticate(username=username, password=password)
+                if user is not None:
+                    return HttpResponseRedirect(reverse('polls:index'))
+                else:
+                    raise ValidationError("Wrong password or email")
         else:
-            raise ValidationError("Wrong password or email")
-    except KeyError:
-        # Réaffiche le formulaire de vote
-        return render(request, 'authcustom/login_page.html')
+            form = LoginForm()
+            return render(request, 'authcustom/login_page.html', {'form': form})
     except ValidationError as ex:
-        return render(request, 'authcustom/login_page.html', {'error_message': ex.message})
+        return render(request, 'authcustom/login_page.html', {'error_message': ex.message, 'form': form})
